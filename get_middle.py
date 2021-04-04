85707d2b3eda45d89642f82c6e873484779d09c8
AlrightErin27 <erinmontybruce@gmail.com>
2021-04-04 13:33:04 -0700

    fin q2

--- get_middle.py
+++ get_middle.py
@@ -1,2 +1,13 @@
+# You are going to be given a word. 
+# Your job is to return the middle character of the word. 
+# If the word's length is odd, return the middle character. 
+# If the word's length is even, return the middle 2 characters.
+
+word1 = "haberdashery" #da
+word2 = "coconut" #o
+
 def get_middle(s):
-    #your code here
+    #take length of s
+    return s[(len(s)-1) // 2 : (len(s)+2) // 2]
+    
+print(get_middle(word1))
