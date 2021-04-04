1f166165beb4f0d41ac1976ee28a21cea8c5ce6a
kchu01 <chukathy01@gmail.com>
2021-04-04 16:16:34 -0700

    finish count pos and sum negs

--- count_positives_sum_negatives.py
+++ count_positives_sum_negatives.py
@@ -1,11 +1,13 @@
 def count_positives_sum_negatives(arr):
     pos_count, neg_sum = [0, 0]
+    neg_arr = []
 
     for x in arr:
-        if x >= 0:
+        if x > 0:
             pos_count += 1
-        elif x < 0:
-            neg_sum = sum(arr)
+        elif x <= 0:
+            neg_arr.append(x)
+            neg_sum = sum(neg_arr)
         else:
             return ([])
 
@@ -13,3 +15,4 @@ def count_positives_sum_negatives(arr):
 
 
 print(count_positives_sum_negatives([1, 2, 3, 4, 5, -11, -12, -13, -14, -15]))
+print(count_positives_sum_negatives([]),[])
