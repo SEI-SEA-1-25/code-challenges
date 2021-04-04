ddd5ecd0aca0cd049ca106c76fb8b2801994712b
kchu01 <chukathy01@gmail.com>
2021-04-04 15:40:30 -0700

    solve pos count function

--- count_positives_sum_negatives.py
+++ count_positives_sum_negatives.py
@@ -1,2 +1,15 @@
 def count_positives_sum_negatives(arr):
-    #your code here
+    pos_count, neg_sum = [0, 0]
+
+    for x in arr:
+        if x >= 0:
+            pos_count += 1
+        elif x < 0:
+            neg_sum = sum(arr)
+        else:
+            return ([])
+
+    return [pos_count, neg_sum]
+
+
+print(count_positives_sum_negatives([1, 2, 3, 4, 5, -11, -12, -13, -14, -15]))
