66c844c2011d6d8e15d408029d068477e39fff63
kchu01 <chukathy01@gmail.com>
2021-04-04 17:56:54 -0700

    include partial soln to is_prime

--- count_positives_sum_negatives.py
+++ count_positives_sum_negatives.py
@@ -1,17 +1,35 @@
+# Attempt 1
+# def count_positives_sum_negatives(arr):
+#     if not arr == None and len(arr) > 0:
+#         pos_count, neg_sum = [0, 0]
+#         neg_arr = []
+
+#         for x in arr:
+#             if x > 0:
+#                 pos_count += 1
+#             elif x <= 0:
+#                 neg_arr.append(x)
+#                 neg_sum = sum(neg_arr)
+#     else:
+#         return ([])
+
+#     return [pos_count, neg_sum]
+
+
+# Attempt 2
 def count_positives_sum_negatives(arr):
-    pos_count, neg_sum = [0, 0]
-    neg_arr = []
-
+    if not arr: 
+        return []
+    pos = 0
+    neg = 0
     for x in arr:
-        if x > 0:
-            pos_count += 1
-        elif x <= 0:
-            neg_arr.append(x)
-            neg_sum = sum(neg_arr)
-        else:
-            return ([])
+      if x > 0:
+          pos += 1
+      if x < 0:
+          neg += x
+    return [pos, neg]
 
-    return [pos_count, neg_sum]
+  
 
 
 print(count_positives_sum_negatives([1, 2, 3, 4, 5, -11, -12, -13, -14, -15]))
