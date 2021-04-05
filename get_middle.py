f3116e78ea6f2c55b65cb5f7c4f70334588d030f
gabrielbustos550 <gabrielbustos550@gmail.com>
2021-04-05 10:53:01 -0600

    hw

--- get_middle.py
+++ get_middle.py
@@ -1,2 +1,11 @@
 def get_middle(s):
     #your code here
+if(len(s) % 2 ) == 1:
+    index = (len(s) - 1) / 2
+    return s[index]
+else:
+    start_index = int(len(s) / 2) - 1
+    end_index = int(start_index + 1)
+    return s[start_index : end_index + 1]
+
+print(get_middle('Testing'))
