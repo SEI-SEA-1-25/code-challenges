25ea95294c3eaef55efb90356af9d2bcddc8d7c0
wonkatin <j.drawers@gmail.com>
2021-04-05 10:45:16 -0500

    solved count pos sum neg, descending order and duplicate count

--- count_positives_sum_negatives.py
+++ count_positives_sum_negatives.py
@@ -1,2 +1,19 @@
 def count_positives_sum_negatives(arr):
     #your code here
+    if arr == []:
+        return []
+    neg_sum = 0
+    pos_sum = 0
+    for num in arr:
+        if num > 0:
+            pos_sum += 1
+            # print(pos_sum)
+        if num < 0:
+            neg_sum += num
+            # print(neg_sum)
+    return [pos_sum, neg_sum]
+
+print (count_positives_sum_negatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]))
+print (count_positives_sum_negatives([0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14]))
+print (count_positives_sum_negatives([]))
+
--- descending_order.py
+++ descending_order.py
@@ -1,2 +1,13 @@
 def descending_order(num):
     # Bust a move right here
+    num2str = str(num)
+    if(len(num2str) <= 1):
+        return int(num2str)
+    else:
+        num_arr = list(num2str)
+        num_arr.sort(reverse = True)
+        # print(num_arr)
+        whatever = int("".join(num_arr))
+        return whatever
+
+print( descending_order(123456789))
