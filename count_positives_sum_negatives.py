527683f7457c482ab441ff4eccb9cba1fdeab492
ChrisB007 <chrisbitoy@gmail.com>
2021-04-05 12:37:42 -0400

    updates

--- count_positives_sum_negatives.py
+++ count_positives_sum_negatives.py
@@ -1,16 +1,13 @@
 def count_positives_sum_negatives(arr):
-    # Begin positive numbers at 0
-    pos_num = 0
-    # Begin negative numbers at 0
-    neg_num = 0
     # Start with an empty array
     nums_array = []
 
     #Loop through the array, get all positive numbers, append them to nums_array, and display the length
     for num in arr:
         if num > 0:
-            pos_num = len(nums_array.append(num))
-            return pos_num
+            pos_num = nums_array.append(num)
+            positives = len(pos_num)
+            return positives
         elif num < 0:
             neg_num += nums_array.append(num)
             return nums_array
@@ -19,16 +16,3 @@ def count_positives_sum_negatives(arr):
 num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]
 
 count_positives_sum_negatives(num)
-
-    
-
-            
-
-
-
-
-#Given an array of integers.
-
-#Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers.
-
-#If the input array is empty or null, return an empty array.
