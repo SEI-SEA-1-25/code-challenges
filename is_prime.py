5f396deb223d855def91ea973214c5b9aba1a725
Joshua <46643961+jcom619@users.noreply.github.com>
2021-04-05 09:37:55 -0700

    Update is_prime.py

--- is_prime.py
+++ is_prime.py
@@ -1,2 +1,10 @@
 def is_prime(num):
-  pass #TODO
+num = 11
+if num > 1:
+    for i in range(2, int(num/2)+1):
+        if (num % i) == 0:
+            return num, True,  "isprime"
+        else:
+            return num, False, "is not prime"
+          
+is_prime(num) 
