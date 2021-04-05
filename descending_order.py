24526acebb2dab594394d72f040495d8104a06c6
AlrightErin27 <erinmontybruce@gmail.com>
2021-04-05 09:39:34 -0700

    attempted descending q

--- descending_order.py
+++ descending_order.py
@@ -1,2 +1,14 @@
+example = [1897201031]
+
 def descending_order(num):
-    # Bust a move right here
+    # convert integers to a sting
+    num_str = str(num)
+    # cast the string to a list
+    num_list = list(num_str)
+    # sort the list
+    sort_nums = num_list.sort()
+    # reverse the list
+    result = sort_nums.revese()
+    return result
+    
+print(descending_order(example))
